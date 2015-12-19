0a33b2f6635f9df910e4fd330187e710925009f2
Leon Chen <leonhart.chen@gmail.com>
2015-12-19 16:42:01 +0800

    Initial Commit

---
+++ simpleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleApp
+//
+//  Created by leon chen on 12/19/15.
+//  Copyright © 2015 leon chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
